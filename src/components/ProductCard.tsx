af7a6355379fe24d267d8ab253fb07226971e33f
ShiroSan <harlampevrenat@gmail.com>
2025-06-15 16:12:32 +0900

    Улучшен UX в карточках каталога

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,7 +22,10 @@ export default function ProductCard({ product }: Props) {
         />
         <Link to={`/product/${product.id}`}>
           <div className="p-4">
-            <h3 className="text-xl font-semibold text-gray-200">{product.name}</h3>
+						<div className="flex justify-between">
+							<h3 className="text-xl font-semibold text-gray-200">{product.name}</h3>
+							<button className='text-gray-400 underline'>Подробнее</button>
+						</div>
             <div className="flex justify-between">
               <p className="text-grayText text-sm w-1/2 my-1">{product.description}</p>
               <p className="font-bold text-green-500 text-lg">{product.price.toLocaleString()} ₽</p>
